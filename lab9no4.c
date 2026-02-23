a8841d11373b806a2d8b5f6110c87463c61c132e
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2026-02-24 01:47:03 +0700

    swap array1d

--- lab9no4.c
+++ lab9no4.c
@@ -8,13 +8,32 @@ int main() {
 	printf("Before swap function: a = %d, b = %d, c = %d\n", a, b, c);
 	swapValue(&a, &b, &c);
 	printf("After swap function: a = %d, b = %d, c = %d\n", a, b, c);
-
+	
+	int i;
+	int arr[4] = {1, 2, 3, 4};
+    int brr[4] = {10, 20, 30, 40};
+    printf("\nBefore swap:\n");
+    printf("arr1: %d %d %d %d\n",arr[0],arr[1],arr[2],arr[3]);
+    printf("arr2: %d %d %d %d\n",brr[0],brr[1],brr[2],brr[3]);
+    swapArray(arr,brr,4);
+    printf("\nAfter swap:\n");
+    printf("arr1: %d %d %d %d\n",arr[0],arr[1],arr[2],arr[3]);
+    printf("arr2: %d %d %d %d\n",brr[0],brr[1],brr[2],brr[3]);
 }
 
-void swapValue(int *a, int *b, int *c) {
+void swapValue(int *a, int *b, int *c){
     int temp = *a; 
     *a = *b;         
     *b = *c;       
     *c = temp;       
+}
+
+void swapArray(int a[], int b[], int size){
+	int i;
+    for (i=0;i<size; i++) {
+        int temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
 }
 
